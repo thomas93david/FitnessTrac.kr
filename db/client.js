ad1672effff41401eade14ac0dff6d595afecfdb
Your Name <your_email@real_email.edu>
2020-08-16 17:22:36 -0400

    Added Heroku

--- db/client.js
+++ db/client.js
@@ -2,7 +2,9 @@
 //  This will be imported into your individual files below.
 const { Client } = require("pg");
 
-const client = new Client("postgres://localhost:5432/fitness-dev");
+const client = new Client(
+  process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev"
+);
 
 module.exports = {
   client,
